118a81ec2a1584f61a25a466fc16f3b1a0bd7201
bruno <bruno.borges222@hotmail.com>
2020-12-29 02:08:52 -0300

    Brincadeira de matematica

---
+++ JOGO DE MATEMATTICA/Numero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace JOGO_DE_MATEMATTICA
+{
+
+    class Numero
+    {
+        public double n1;
+
+        public void testando()
+        {
+            while (n1 > 10 )
+            {
+                Console.WriteLine("Digite um número de 1 a 10.");
+                n1 = double.Parse(Console.ReadLine());
+            }
+        }
+
+        public void tecla()
+        {
+            ConsoleKeyInfo keyinfo;
+
+
+            do
+            {
+                keyinfo = Console.ReadKey(true);
+
+                if (keyinfo.Key != ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Aperte enter, por favor");
+
+                }
+            } while (keyinfo.Key != ConsoleKey.Enter);
+
+
+        }
+
+        public double resultado()
+        {
+            return n1 / 2;
+        }
+
+        public override string ToString()
+        {
+            return "O resultado de tudo é " + resultado();
+        }
+    }
+}
---
+++ JOGO DE MATEMATTICA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JOGO_DE_MATEMATTICA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero num = new Numero();
+
+            Console.WriteLine("Digite um número de 1 a 10.");
+            num.n1 = double.Parse(Console.ReadLine());
+            num.testando();
+            Console.WriteLine("Pense em um número e aperte enter.");
+            num.tecla();
+            Console.WriteLine("Multiplica por 2 e aperte enter.");
+            num.tecla();
+            Console.WriteLine("Mais o número que você escolheu e aperte enter.");
+            num.tecla();
+            Console.WriteLine("Dividido por 2 e aperte enter.");
+            num.tecla();
+            Console.WriteLine("Menos o número que você pensou e aperte enter.");
+            num.tecla();
+            Console.WriteLine(num.resultado());
+
+            Console.ReadLine();
+            return;
+        }
+    }
+}
